d9c149b78f06d645b208a7cc750d176ff2c695f4
Larry Guger <larrygug@microsoft.com>
2017-03-24 15:05:05 -0700

    added send message call

--- f12sample.js
+++ f12sample.js
@@ -12,4 +12,23 @@ function getServiceWorkerDetails() {
         }
     });
 
+    sendMessage("test").then(function(data){
+        console.log(data);
+    });
+
+}
+
+function sendMessage(message) {
+    return new Promise(function(resolve, reject) {
+        var messageChannel = new MessageChannel();
+        messageChannel.port1.onmessage = function(event) {
+            if(event.data.error) {
+                reject(event.data.error);
+            } else {
+                resolve(event.data);
+            }
+        };
+
+        navigator.serviceWorker.controller.postMessage(message, [messageChannel.port2]);
+    });
 }
